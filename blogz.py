127b1de71a440d0cf647e622bb34c73d2668822b
James Flores <jamesflores@ha21457falk.claytonschools.stl>
2017-10-15 21:00:29 -0500

    Initial commit with new DB info.

--- blogz.py
+++ blogz.py
@@ -6,7 +6,7 @@ app.config['DEBUG'] = True
 
 # Note: the connection string after :// contains the following info:
 # user:password@server:portNumber/databaseName
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://build-a-blog:blogfrog8@localhost:8889/build-a-blog'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://blogz:unit2wrapup@localhost:8889/build-a-blog'
 app.config['SQLALCHEMY_ECHO'] = True
 db = SQLAlchemy(app)
 
